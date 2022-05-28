39a57800aa37f6de0c04cc99236350d919df0ff5
Romain Auger <romainauger@wanadoo.fr>
2022-05-28 18:02:36 +0200

    Header + Footer with CSS for each

--- Views/Templates/header.php
+++ Views/Templates/header.php
@@ -4,11 +4,11 @@
     <h1><a href="index.php">MultiMédiasThèque</a></h1>
     <section>
         
-            <a href="?page=register">REGISTER</a>
-            <a href="?page=login">LOGIN</a>
+            <a href="?page=register">Sign Up</a>
+            <a href="?page=login">Log in</a>
     
-            <a href="?page=account">Account</a>
-            <a href="../Views/logout.php">Logout</a>
+            <!-- <a href="?page=account">Account</a>
+            <a href="../Views/logout.php">Logout</a> -->
  
     </section>
 </header>
